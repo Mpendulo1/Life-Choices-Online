46606ed6646fe21f34ca5695ba0357c7395ca04c
lifechoices <mpendulokhozamk22gmail.com>
2021-07-06 20:18:24 +0200

    updated changes

--- main.py
+++ main.py
@@ -33,34 +33,54 @@ class MainScreen:
         # self.admin_button.place(x=390, y=350)
 
     def DatabaseLogin(self):
-        if self.username_entry.get() == '':
-            messagebox.showerror('Enter Username', 'Please Enter A Username')
-        elif self.password_entry.get() == '':
-            messagebox.showerror('Enter Password', 'Please Enter A Valid Password')
-        elif self.password_entry.get() == '' or self.username_entry.get() == '':
-            messagebox.showerror('Inputs Empty', 'Please Enter Valid Details!')
+        global userID
+        try:
+            connection = mysql.connector.connect(user='sql6423123', password='EYQeBzmlvU', port='3306', host='sql6.freesqldatabase.com', database='sql6423123', auth_plugin='mysql_native_password')
+            curs = connection.cursor()
+        except:
+            print('You are not connected to the server')
         else:
-            my_database = mysql.connector.connect(user='sql6423123', password='EYQeBzmlvU', port='3306', host='sql6.freesqldatabase.com', database='sql6423123', auth_plugin='mysql_native_password')
-            mycursor = my_database.cursor()
-            a = mycursor.execute('SELECT * FROM Life Choices Online')
-
-            for i in mycursor:
-                if i[0] == self.username_entry.get() or i[1] == self.password_entry.get():
-                    messagebox.Message('Login Successfully', 'Enjoy Your Day')
+            name = self.username_entry.get()
+            password = self.password_entry.get()
+            curs = connection.cursor()
+            query = ('SELECT Name, Password FROM `Life Choices Online` ')
+            for (Name, Password) in curs:
+                if name == Name and password == Password:
+                    messagebox.showinfo('Welcome' + name, 'Login Successful, Enjoy Your Day')
+                    curs.close()
+                    connection.close()
                     root.destroy()
 
-                if i[0] != self.username_entry.get():
-                    messagebox.showerror('Invalid Username', 'Please Enter A Correct Username!')
-                    self.username_entry.delete(0, END)
-                elif i[1] != self.password_entry.get():
-                    messagebox.showerror('Invalid Password', 'Please Enter A Correct Password')
-                    self.password_entry.delete(0, END)
-                else:
-                    i[1] != self.username_entry or i[0] != self.password_entry.get()
-                    messagebox.RETRYCANCEL('Invalid Entries', "Click 'RETRY' to Re-Enter '\n' CLick 'CANCEL' to Go And Register")
-                    self.username_entry.delete(0, END)
-                    self.password_entry.delete(0, END)
-                    return
+
+
+        # if self.username_entry.get() == '':
+        #     messagebox.showerror('Enter Username', 'Please Enter A Username')
+        # elif self.password_entry.get() == '':
+        #     messagebox.showerror('Enter Password', 'Please Enter A Valid Password')
+        # elif self.password_entry.get() == '' or self.username_entry.get() == '':
+        #     messagebox.showerror('Inputs Empty', 'Please Enter Valid Details!')
+        # else:
+        #     my_database = mysql.connector.connect(user='sql6423123', password='EYQeBzmlvU', port='3306', host='sql6.freesqldatabase.com', database='sql6423123', auth_plugin='mysql_native_password')
+        #     mycursor = my_database.cursor()
+        #     a = mycursor.execute('SELECT * FROM Life Choices Online')
+        #
+        #     for i in mycursor:
+        #         if i[0] == self.username_entry or i[1] == self.password_entry.get():
+        #             messagebox.Message('Login Successfully', 'Enjoy Your Day')
+        #             root.destroy()
+        #
+        #         if i[0] != self.username_entry.get():
+        #             messagebox.showerror('Invalid Username', 'Please Enter A Correct Username!')
+        #             self.username_entry.delete(0, END)
+        #         elif i[1] != self.password_entry.get():
+        #             messagebox.showerror('Invalid Password', 'Please Enter A Correct Password')
+        #             self.password_entry.delete(0, END)
+        #         else:
+        #             i[1] != self.name_entry or i[0] != self.password_entry.get()
+        #             messagebox.RETRYCANCEL('Invalid Entries', "Click 'RETRY' to Re-Enter '\n' CLick 'CANCEL' to Go And Register")
+        #             self.username_entry.delete(0, END)
+        #             self.password_entry.delete(0, END)
+        #             return
 
     def register(self):
         root.destroy()
--- register.py
+++ register.py
@@ -1,9 +1,10 @@
 import tkinter
 from tkinter import *
+from tkinter import messagebox
 import tkinter as ttk
 import mysql.connector
 master = Tk()
-master.geometry('700x600')
+master.geometry('700x700')
 master.title('Register Username')
 
 class Registration:
@@ -23,57 +24,68 @@ class Registration:
         self.surname_entry = ttk.Entry(master, textvariable=self.input_text, justify=CENTER, font=('courier', 20), bd=1)
         self.surname_entry.place(x=250, y=170)
 
+        self.Password = Label(master, text='Password', foreground='black', font=('Arial', 20))
+        self.Password.place(x=20, y=240)
+        self.Password_entry = ttk.Entry(master, textvariable=self.input_text, justify=CENTER, font=('courier', 20), bd=1)
+        self.Password_entry.place(x=250, y=240)
+
         self.ID_number = Label(master, text='ID Number', foreground='black', font=('Arial', 20))
-        self.ID_number.place(x=20, y=240)
+        self.ID_number.place(x=20, y=310)
         self.ID_number_entry = ttk.Entry(master, textvariable=self.input_text, justify=CENTER, font=('courier', 20), bd=1)
-        self.ID_number_entry.place(x=250, y=240)
+        self.ID_number_entry.place(x=250, y=310)
 
         self.Phone_number = Label(master, text='Phone Number', foreground='black', font=('Arial', 20))
-        self.Phone_number.place(x=20, y=310)
+        self.Phone_number.place(x=20, y=380)
         self.Phone_number_entry = ttk.Entry(master, textvariable=self.input_text, justify=CENTER, font=('courier', 20), bd=1)
-        self.Phone_number_entry.place(x=250, y=310)
+        self.Phone_number_entry.place(x=250, y=380)
 
         self.kin_name = Label(master, text="Next of kin's Name", foreground='black', font=('Arial', 20))
-        self.kin_name.place(x=20, y=380)
+        self.kin_name.place(x=20, y=450)
         self.kin_name_entry = ttk.Entry(master, textvariable=self.input_text, justify=CENTER, font=('courier', 20), bd=1)
-        self.kin_name_entry.place(x=250, y=380)
+        self.kin_name_entry.place(x=250, y=450)
 
         self.kin_number = Label(master, text="Next of kin's Cell", foreground='black', font=('Arial', 20))
-        self.kin_number.place(x=20, y=450)
+        self.kin_number.place(x=20, y=520)
         self.kin_number_entry = ttk.Entry(master, textvariable=self.input_text, justify=CENTER, font=('courier', 20), bd=1)
-        self.kin_number_entry.place(x=250, y=450)
+        self.kin_number_entry.place(x=250, y=520)
 
         self.register_btn = tkinter.Button(master, text='Register', relief=GROOVE, activebackground="#33B5E5", bg='gray80', fg='gray12', font=('Georgia', 20, 'bold'), cursor='hand2', command=self.database_register)
-        self.register_btn.place(x=150, y=530)
+        self.register_btn.place(x=150, y=600)
 
-        self.register_btn = tkinter.Button(master, text='Login', relief=GROOVE, activebackground="#33B5E5", bg='gray80', fg='gray12', font=('Georgia', 20, 'bold'), cursor='hand2', command=self.BackToExit)
-        self.register_btn.place(x=350, y=530)
+        self.register_btn = tkinter.Button(master, text='Login', relief=GROOVE, activebackground="#33B5E5", bg='gray80', fg='gray12', font=('Georgia', 20, 'bold'), cursor='hand2', command=self.BacktoExit)
+        self.register_btn.place(x=350, y=600)
 
-    def BackToExit(self):
-
+    def BacktoExit(self):
         master.destroy()
         import main
 
-
     def database_register(self):
+        name = self.name_entry.get()
+        surname = self.surname_entry.get()
+        password = self.Password_entry.get()
+        identity = self.ID_number_entry.get()
+        phone_number = self.Phone_number_entry.get()
+        kin_name = self.kin_name_entry.get()
+        kin_cell = self.kin_number_entry.get()
 
-        if self.name_entry.get() == '' or self.ID_number_entry.get() == '' or self.surname_entry.get() == '' or self.Phone_number_entry.get() == '':
-            self.messagebox.showerror('STATUS', "PLEASE Enter Valid Details")
+        if self.name_entry.get() == '' or self.ID_number_entry.get() == '' or self.surname_entry.get() == '' or self.Phone_number_entry.get() == '' or self.Password_entry.get() == '':
+            messagebox.showerror('STATUS', "PLEASE Enter Valid Details")
         else:
-            mydb = mysql.connector.connect(user='sql6423123', password='@EYQeBzmlvU', port='3306', host='sql6.freesqldatabase.com', database='https://www.freesqldatabase.com/', auth_plugin='mysql_native_password')
-
-            mycursor = mydb.cursor()
-            sql = "INSERT INTO Login (Name, Surname, ID Number, Phone Number, next of kin's Name, next of kin's Number) VALUE (%s, %s, %s, %s, %s, %s)"
-            val = (self.username_entry.get(), self.password_entry.get())
-            mycursor.execute(sql, val)
-            self.messagebox.showinfo("PERMISSION", "YOU HAVE REGISTERED SUCCESSFULLY")
+            con = mysql.connector.connect(user='sql6423123', password='EYQeBzmlvU', port='3306', host='sql6.freesqldatabase.com', database='sql6423123', auth_plugin='mysql_native_password')
+            cursor = con.cursor()
+            cursor.execute("INSERT INTO `Life Choices Online` VALUES('" + name + "','" + password + "', '" + surname + "', '" + identity + "', '" + phone_number + "', '" + kin_name + "', '" + kin_cell + "' )")
+            cursor.execute('commit')
+            messagebox.showinfo("PERMISSION", "YOU HAVE REGISTERED SUCCESSFULLY")
             self.name_entry.delete(0, END)
             self.surname_entry.delete(0, END)
+            self.Password_entry.delete(0, END)
             self.ID_number_entry.delete(0, END)
             self.Phone_number_entry.delete(0, END)
             self.kin_name_entry.delete(0, END)
             self.kin_number_entry.delete(0, END)
-            mydb.commit()
+            con.close();
+            master.destroy()
+            import main
 
 
 
